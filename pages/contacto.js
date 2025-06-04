22f4f2e88e3b0db51535e1a4f1824e45e562c862
fmoscato40 <fernandomoscato16@gmail.com>
2025-06-04 16:43:04 -0300

    Update contacto.js

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,1 +1,8 @@
-// Página Contacto
+export default function Contacto() {
+  return (
+    <div>
+      <h1>Contacto</h1>
+      <p>Escribinos a administracion@motta.com.ar o visitanos en Rioja 540, piso 2, Rosario.</p>
+    </div>
+  );
+}
