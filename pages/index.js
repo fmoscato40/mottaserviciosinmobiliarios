517b7a10b2bfca10549436f85e54cd93766d9e81
fmoscato40 <fernandomoscato16@gmail.com>
2025-06-04 16:41:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-// Página de inicio
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenidos a Motta Servicios Inmobiliarios</h1>
+      <p>El camino hacia tu nuevo hogar empieza acá.</p>
+    </div>
+  );
+}
