80d18ef42b74f4b1562eca94bbee2aff752fcdc0
fmoscato40 <fernandomoscato16@gmail.com>
2025-06-04 16:42:46 -0300

    Update tasaciones.js

--- pages/tasaciones.js
+++ pages/tasaciones.js
@@ -1,1 +1,8 @@
-// Página Tasaciones
+export default function Tasaciones() {
+  return (
+    <div>
+      <h1>Tasaciones</h1>
+      <p>Solicitá una tasación profesional para conocer el valor real de tu propiedad.</p>
+    </div>
+  );
+}
