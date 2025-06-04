c40c8320ae58cec0796563ab7ca9cf4921717701
fmoscato40 <fernandomoscato16@gmail.com>
2025-06-04 16:42:23 -0300

    Update servicios.js

--- pages/servicios.js
+++ pages/servicios.js
@@ -1,1 +1,8 @@
-// Página Servicios
+export default function Servicios() {
+  return (
+    <div>
+      <h1>Servicios</h1>
+      <p>Ofrecemos ventas, alquileres, tasaciones y asesoramiento profesional.</p>
+    </div>
+  );
+}
